2f02e1988272a58c8b1332723419b341cc8996de
salaudeen22 <salaudeensalu@gmail.com>
2024-05-07 22:43:28 +0530

    done with export as pdf option enabled ,display all urls when click on checkbox.minor function updated

--- background.js
+++ background.js
@@ -1,127 +1,152 @@
-const tabTimeObjectKey = "tabTimesObject"; // { key:url, value: {url:string, trackedSeconds: number, lastDateVal: number //utc milliseconds?,startDateVal:number}}
-const lastActiveTabKey = "lastActiveTab"; // {url:string, lastDateVal: number}
+const tabTimeObjectKey = "tabTimesObject"; // { key:url, value: {url:string, trackedSeconds: number, lastDateVal: number //utc milliseconds?,startDateVal:number,urlDetails:string[]}}
+const lastActiveTabKey = "lastActiveTab"; // {url:string, lastDateVal: number,fullUrl:String}
 
-chrome.runtime.onInstalled.addListener(function() {
-    console.log("Extension installed or updated.");
-  
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: {},
-                    })
-                ],
-                actions: [ new chrome.declarativeContent.ShowPageAction() ]
-            }
-        ]);
-    });
+chrome.runtime.onInstalled.addListener(function () {
+  console.log("Extension installed or updated.");
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {},
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+  });
 });
 
-chrome.windows.onFocusChanged.addListener(function(windowId) {
-    if (windowId === chrome.windows.WINDOW_ID_NONE) {
-        // Reset the last active tab data if the window loses focus
-        resetLastActiveTab();
-    } else {
-        // Process tab change if the window gains focus
-        processTabChange(true);
-    }
+chrome.windows.onFocusChanged.addListener(function (windowId) {
+  if (windowId === chrome.windows.WINDOW_ID_NONE) {
+    // Reset the last active tab data if the window loses focus
+    resetLastActiveTab();
+  } else {
+    // Process tab change if the window gains focus
+    processTabChange(true);
+  }
 });
 
 function resetLastActiveTab() {
-    chrome.storage.local.set({ [lastActiveTabKey]: '' }, function() {
-        console.log("Last active tab reset.");
-    });
+  chrome.storage.local.set({ [lastActiveTabKey]: "" }, function () {
+    console.log("Last active tab reset.");
+  });
 }
 
 function processTabChange(isWindowActive) {
-  
-    chrome.tabs.query({ active: true }, function(tabs) {
-        if (tabs.length > 0 && tabs[0] !== null) {
-            let currentTab = tabs[0];
-            let url = currentTab.url;
-            let title = currentTab.title;
-            let hostName = url;
-            try {
-                let urlObject = new URL(url);
+  chrome.tabs.query({ active: true }, function (tabs) {
+    if (tabs.length > 0 && tabs[0] !== null) {
+      let currentTab = tabs[0];
+      let url = currentTab.url;
+     
+      let title = currentTab.title;
+      let hostName = url;
+      try {
+   
 
-                hostName = urlObject.hostname;
-            } catch (e) {
-                console.log(e);
-                // console.error(`Could not construct URL from ${currentTab.url}, error: ${e}`);
+
+        let urlObject = new URL(hostName);
+
+        hostName = urlObject.hostname;
+        console.log("hostname"+hostName);
+      } catch (e) {
+        console.log(e);
+        // console.error(`Could not construct URL from ${currentTab.url}, error: ${e}`);
+      }
+      console.log("isWindowActive: " + isWindowActive+"url:"+url);
+      console.log(tabs);
+
+      chrome.storage.local.get(
+        [tabTimeObjectKey, lastActiveTabKey],
+        function (result) {
+          let lastActiveTab = JSON.parse(result[lastActiveTabKey] || "{}");
+          let tabTimeObject = JSON.parse(result[tabTimeObjectKey] || "{}");
+          console.log("result:" + JSON.stringify(result));
+
+          if (
+            lastActiveTab.hasOwnProperty("url") &&
+            lastActiveTab.hasOwnProperty("lastDateVal")
+          ) {
+            let lastUrl = lastActiveTab["url"];
+            let currentDateVal = Date.now();
+            let passedSeconds =
+              (currentDateVal - lastActiveTab["lastDateVal"]) * 0.001; // Convert ms to s
+
+            let fullUrl = lastUrl;
+            if (lastActiveTab.hasOwnProperty("fullUrl")) {
+              fullUrl = lastActiveTab["fullUrl"];
             }
-            console.log("isWindowActive: " + isWindowActive);
-            console.log(tabs);
 
-           
-            chrome.storage.local.get([tabTimeObjectKey, lastActiveTabKey], function(result) {
-                let lastActiveTab = JSON.parse(result[lastActiveTabKey] || '{}');
-                let tabTimeObject = JSON.parse(result[tabTimeObjectKey] || '{}');
-                console.log("result:"+JSON.stringify(result));
+            if (tabTimeObject.hasOwnProperty(lastUrl)) {
+              let lastUrlObjectInfo = tabTimeObject[lastUrl];
+              if (lastUrlObjectInfo.hasOwnProperty("trackedSeconds")) {
+                lastUrlObjectInfo["trackedSeconds"] += passedSeconds;
+              } else {
+                lastUrlObjectInfo["trackedSeconds"] = passedSeconds;
+              }
+              lastUrlObjectInfo["lastDateVal"] = currentDateVal;
+              if (lastUrlObjectInfo.hasOwnProperty("urlDetails")) {
+                let detailUrlArr = lastUrlObjectInfo["urlDetails"];
 
-                if (lastActiveTab.hasOwnProperty("url") && lastActiveTab.hasOwnProperty("lastDateVal")) {
-                    let lastUrl = lastActiveTab["url"];
-                    let currentDateVal = Date.now();
-                    let passedSeconds = (currentDateVal - lastActiveTab["lastDateVal"]) * 0.001; // Convert ms to s
-                    if (tabTimeObject.hasOwnProperty(lastUrl)) {
-                        let lastUrlObjectInfo = tabTimeObject[lastUrl];
-                        if (lastUrlObjectInfo.hasOwnProperty("trackedSeconds")) {
-                            lastUrlObjectInfo["trackedSeconds"] += passedSeconds;
-                        } else {
-                            lastUrlObjectInfo["trackedSeconds"] = passedSeconds;
-                        }
-                    } else {
-                        
-                        let newUrlInfo = {
-                            url: lastUrl,
-                            trackedSeconds: passedSeconds,
-                            lastDateVal: currentDateVal,
-                            startDateVal: lastActiveTab["lastDateVal"]
-                        };
-                        tabTimeObject[lastUrl] = newUrlInfo;
-                    }
+                if (detailUrlArr.indexOf(fullUrl) > 0) {
+                  detailUrlArr.push(fullUrl);
+                } else {
+                  lastUrlObjectInfo["urlDetails"] = [fullUrl];
                 }
+              }
+            } else {
+              let newUrlInfo = {
+                url: lastUrl,
+                trackedSeconds: passedSeconds,
+                lastDateVal: currentDateVal,
+                startDateVal: lastActiveTab["lastDateVal"],
+                urlDetails: [fullUrl],
+              };
+              tabTimeObject[lastUrl] = newUrlInfo;
+            }
+          }
 
-               
-                let currentDateValue = Date.now();
-                let lastTabInfo = {
-                    "url": hostName ? hostName : "Unknown Tab",
-                    "lastDateVal": currentDateValue
-                };
-                
-                if (!isWindowActive) {
-                    lastTabInfo = {};
-                }
+          let currentDateValue = Date.now();
+          let lastTabInfo = {
+            url: hostName ? hostName : "Unknown Tab",
+            lastDateVal: currentDateValue,
+            fullUrl: url,
+          };
 
-               
-                let newLastTabObject = {};
-                newLastTabObject[lastActiveTabKey] = JSON.stringify(lastTabInfo);
+          if (!isWindowActive) {
+            lastTabInfo = {};
+          }
 
-             
-                chrome.storage.local.set(newLastTabObject, function() {
-                    console.log("Last active tab stored: " + hostName);
-                });
+          let newLastTabObject = {};
+          newLastTabObject[lastActiveTabKey] = JSON.stringify(lastTabInfo);
 
-               
-                const tabTimesObjectString = JSON.stringify(tabTimeObject);
-                let newTabTimesObject = {};
-                newTabTimesObject[tabTimeObjectKey] = tabTimesObjectString;
+          chrome.storage.local.set(newLastTabObject, function () {
+            console.log("Last active tab stored: " + hostName);
+          });
 
-           
-                chrome.storage.local.set(newTabTimesObject, function() {
-                    console.log("Tab times object updated.");
-                });
-            });
+          const tabTimesObjectString = JSON.stringify(tabTimeObject);
+          let newTabTimesObject = {};
+          newTabTimesObject[tabTimeObjectKey] = tabTimesObjectString;
+
+          chrome.storage.local.set(newTabTimesObject, function () {
+            console.log("Tab times object updated.");
+          });
         }
-    });
+      );
+    }
+    else
+    {
+      console.log("no active tab found");
+    }
+  });
 }
 function onTabTrack(activeInfo) {
-    let tabId = activeInfo.tabId;
-    let windowId = activeInfo.windowId;
-    // Perform actions based on the active tab and windowId
-    processTabChange(true); 
+  let tabId = activeInfo.tabId;
+  let windowId = activeInfo.windowId;
+  // Perform actions based on the active tab and windowId
+  processTabChange(true);
 }
 
 // Add listener for tab activation event
 chrome.tabs.onActivated.addListener(onTabTrack);
-
--- popup.js
+++ popup.js
@@ -1,87 +1,172 @@
-let showTableBtn = document.getElementById('btnShowTable');
-let clearTimesBtn = document.getElementById('btnClearTimes');
-let errorMessageElement = document.getElementById('errorMessage');
+let showTableBtn = document.getElementById("btnShowTable");
+let clearTimesBtn = document.getElementById("btnClearTimes");
+let errorMessageElement = document.getElementById("errorMessage");
+let exportPdfBtn = document.getElementById("btnExportPDf");
 let timeTable = document.getElementById("timeTable");
+let allUrlCheckBox = document.getElementById("allUrlCheckBox");
 
-clearTimesBtn.onclick = function(element) {
-    chrome.storage.local.set({ "tabTimesObject": "{}" }, function() {
-        console.log("Tab times cleared.");
-      
-    });
+function download(filename, text) {
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+  element.style.display = "none";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+clearTimesBtn.onclick = function (element) {
+  chrome.storage.local.set({ tabTimesObject: "{}" }, function () {
+    console.log("Tab times cleared.");
+  });
 };
 
-showTableBtn.onclick = function(element) {
-    chrome.storage.local.get("tabTimesObject", function(dataCont) {
-        console.log(dataCont);
-        let dataString = dataCont["tabTimesObject"];
-        if (dataString == null) {
-            return;
+// exportPdfBtn.onclick = function(element) {
+//     let content = "aldkfjsaldkfjalskofjosldfjabldskfjsoljf";
+//     let downloadUrl = "./";
+//     let filename = "my-document.pdf";
+
+//     browser.downloads.download({
+//         url: downloadUrl,
+//         filename: filename,
+//         saveAs: true,
+//         body: content,
+//         conflictAction: "uniquify"
+//     }).then((downloadId) => {
+//         console.log("PDF successfully downloaded with ID: " + downloadId);
+//     }).catch((error) => {
+//         console.error("PDF export was not successful: ", error);
+//     });
+// };
+
+exportPdfBtn.onclick = function (element) {
+  let downloadUrl = "./";
+  const titleText =
+    "Tab-time-trackings-" + encodeURIComponent(new Date().toISOString());
+  let content = titleText + "\r\n\r\n";
+  const paddings = [40, 10, 12, 35, 35];
+  let rowCount = timeTable.rows.length;
+  for (var x = 0; x < rowCount; x++) {
+    let row = timeTable.rows[x];
+    for (var j = 0; j < row.cells.length; j++) {
+      let col = row.cells[j];
+      content += col.innerText.padEnd(j >= paddings.length ? 40 : paddings[j]);
+    }
+    content += "\n";
+  }
+  download(titleText + ".txt", content);
+};
+
+showTableBtn.onclick = function (element) {
+    const ShowAllurl = allUrlCheckBox.checked;
+  chrome.storage.local.get("tabTimesObject", function (dataCont) {
+    console.log(dataCont);
+    let dataString = dataCont["tabTimesObject"];
+    if (dataString == null) {
+      return;
+    }
+    try {
+      let data = JSON.parse(dataString);
+
+      timeTable.innerHTML = "";
+
+      let entries = [];
+      for (let key in data) {
+        if (data.hasOwnProperty(key)) {
+          entries.push(data[key]);
         }
-        try {
-            let data = JSON.parse(dataString);
-          
-            timeTable.innerHTML = "";
-          
-            let entries = [];
-            for (let key in data) {
-                if (data.hasOwnProperty(key)) {
-                    entries.push(data[key]);
-                }
-            }
-           
-            entries.sort(function(el1, el2) {
-                let e1 = el1["trackedSeconds"];
-                let e2 = el2["trackedSeconds"];
-                if (isNaN(e1) || isNaN(e2)) {
-                    return 0;
-                }
-                if (e1 > e2) {
-                    return -1;
-                } else if (e1 < e2) {
-                    return 1;
-                }
-                return 0;
-            });
-           
-            entries.forEach(function(urlObject) {
-                console.log(urlObject);
-                let newRow = timeTable.insertRow(0); 
-                let celHostname = newRow.insertCell(0);
-                let celTimeMinutes = newRow.insertCell(1);
-                let celTime = newRow.insertCell(2);
-                let celLastDate = newRow.insertCell(3);
-                let celFirstDate = newRow.insertCell(4);
-                celHostname.innerHTML = urlObject["url"];
-                let time_ = urlObject["trackedSeconds"] != null ? urlObject["trackedSeconds"] : 0;
-                celTime.innerHTML = Math.round(time_);
-                celTimeMinutes.innerHTML = (time_ / 60).toFixed(2);
-                let date = new Date();
-                if (urlObject.hasOwnProperty(["lastDateVal"])) {
-                    date.setTime(urlObject["lastDateVal"]);
-                    celLastDate.innerHTML = date.toUTCString();
-                  } else {
-                    celLastDate.innerHTML = "date not found ";
-                  }
-                  if (urlObject.hasOwnProperty("startDateVal")) {
-                    console.log("frist name:"+ date.toUTCString());
-                    date.setTime(urlObject["startDateVal"]);
-                    celFirstDate.innerHTML = date.toUTCString();
-                  } else {
-                    celFirstDate.innerHTML = "date not found ";
-                  }
-          
-            });
-            
-            let headerRow = timeTable.insertRow(0);
-            headerRow.insertCell(0).innerHTML = "Url";
-            headerRow.insertCell(1).innerHTML = "Minutes";
-            headerRow.insertCell(2).innerHTML = "Tracked Seconds";
-            headerRow.insertCell(3).innerHTML = "Last Date";
-            headerRow.insertCell(4).innerHTML = "Frist Date";
-        } catch (err) {
-            const message = "Loading the tabTimesObject went wrong: " + err.toString();
-            console.error(message);
-            errorMessageElement.innerText = message;
+      }
+
+      entries.sort(function (el1, el2) {
+        let e1 = el1["trackedSeconds"];
+        let e2 = el2["trackedSeconds"];
+        if (isNaN(e1) || isNaN(e2)) {
+          return 0;
         }
-    });
+        if (e1 > e2) {
+          return -1;
+        } else if (e1 < e2) {
+          return 1;
+        }
+        return 0;
+      });
+
+
+
+      entries.forEach(function (urlObject) {
+        // console.log(urlObject);
+        let urlobj= urlObject["url"]
+
+        if (ShowAllurl && urlObject.hasOwnProperty("urlDetails")) {
+            let urlDetails = urlObject["urlDetails"];
+            console.log(urlDetails);
+          
+            const ul = document.createElement("ul");
+          
+            urlDetails.forEach((url) => {
+              const li = document.createElement("li"); 
+              li.textContent = url;
+              ul.appendChild(li); 
+            });
+          
+            let newRow = timeTable.insertRow(0);
+            let celHostname = newRow.insertCell(0);
+            celHostname.innerHTML = urlObject["url"]; 
+          
+           
+            let detailsCell = newRow.insertCell(1);
+            detailsCell.appendChild(ul);
+          
+           
+          }
+          else {
+          let newRow = timeTable.insertRow(0);
+          let celHostname = newRow.insertCell(0);
+          let celTimeMinutes = newRow.insertCell(1);
+          let celTime = newRow.insertCell(2);
+          let celLastDate = newRow.insertCell(3);
+          let celFirstDate = newRow.insertCell(4);
+
+          celHostname.innerHTML = urlobj;
+
+          let time_ =
+            urlObject["trackedSeconds"] != null
+              ? urlObject["trackedSeconds"]
+              : 0;
+          celTime.innerHTML = Math.round(time_);
+          celTimeMinutes.innerHTML = (time_ / 60).toFixed(2);
+          let date = new Date();
+          if (urlObject.hasOwnProperty(["lastDateVal"])) {
+            date.setTime(urlObject["lastDateVal"]);
+            celLastDate.innerHTML = date.toUTCString();
+          } else {
+            celLastDate.innerHTML = "date not found ";
+          }
+          if (urlObject.hasOwnProperty("startDateVal")) {
+            console.log("frist name:" + date.toUTCString());
+            date.setTime(urlObject["startDateVal"]);
+            celFirstDate.innerHTML = date.toUTCString();
+          } else {
+            celFirstDate.innerHTML = "date not found ";
+          }
+        }
+      });
+      if (!ShowAllurl) {
+    
+      let headerRow = timeTable.insertRow(0);
+      headerRow.insertCell(0).innerHTML = "Url";
+      headerRow.insertCell(1).innerHTML = "Minutes";
+      headerRow.insertCell(2).innerHTML = "Tracked Seconds";
+      headerRow.insertCell(3).innerHTML = "Last Date";
+      headerRow.insertCell(4).innerHTML = "Frist Date";
+    } 
+}catch (err) {
+      const message =
+        "Loading the tabTimesObject went wrong: " + err.toString();
+      console.error(message);
+      errorMessageElement.innerText = message;
+    }
+  });
 };
